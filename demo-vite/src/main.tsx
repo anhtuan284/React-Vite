6decf1114cfa8b88d7d4fb49f4b7542515b678b5
anhtuan284 <peteralwaysloveu@gmail.com>
2023-10-27 23:45:46 +0700

    mainf

--- demo-vite/src/main.tsx
+++ demo-vite/src/main.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-
+import MyComponent from './component/MyComponent.tsx'
+import PackingList from './component/PackingList.tsx'
+import ListPeople from './component/ListPeople.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
   <>
-    <App />
+    {/* <App /> */}
+    <PackingList />
+    <ListPeople></ListPeople>
   </>
   // </React.StrictMode>,
 )
